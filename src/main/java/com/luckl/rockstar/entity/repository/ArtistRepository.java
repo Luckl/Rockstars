4933277b348290c7e839b5d8f419dbdd545f6e9e
Luckl <luc.klaassen@gmail.com>
2020-01-19 11:22:42 +0100

    created custom query to search artist by song genres

--- src/main/java/com/luckl/rockstar/entity/repository/ArtistRepository.java
+++ src/main/java/com/luckl/rockstar/entity/repository/ArtistRepository.java
@@ -1,9 +1,16 @@
 package com.luckl.rockstar.entity.repository;
 
 import com.luckl.rockstar.entity.Artist;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "artists", path= "artists")
-public interface ArtistRepository extends CrudRepository<Artist, Integer> {
+public interface ArtistRepository extends PagingAndSortingRepository<Artist, Integer> {
+
+    @Query("SELECT s FROM Artist a, IN(a.songs) s WHERE s.genre=:genre")
+    List<Artist> findArtistsBySongGenre(@Param("genre") String genre);
 }
